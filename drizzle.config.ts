183f3c519a19b7a464e80b9daffaf3ae6f3ea77e
Joshua Maffuccio <joshuamaffuccio@gmail.com>
2023-07-28 14:13:42 -0700

    Add endpoints

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv'
+dotenv.config({ path: '.env.local' })
+import type { Config } from 'drizzle-kit'
+
+export default {
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
+  driver: "mysql2",
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL as string,
+  }
+
+} satisfies Config
